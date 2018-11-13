7adf9fc799ce7fd067b0dd360fd4e18751ad5902
michele-mevi <michele.mevi@mail.polimi.it>
2018-11-13 18:05:06 +0100

    ladder mechanics

--- BobTheChameleon/Assets/Scripts/PlayerMovement.cs
+++ BobTheChameleon/Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,45 @@ public class PlayerMovement : MonoBehaviour
 
     public CharacterController2D controller;
 
+
     public float runSpeed = 40f;
+    private float climbSpeed =20f;//climbSpeed=0.5*runSpeed;
 
     float horizontalMove = 0f;
+    float verticalMove = 0f;
     bool jump = false;
 
     public bool isSwinging;
+    public bool isOnLadder;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump"))
+        if (isOnLadder)
         {
-            jump = true;
+            verticalMove = Input.GetAxisRaw("Vertical") * climbSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+
         }
-        else if(Input.GetButtonDown("Sprint"))
-            runSpeed *= 1.5f;
-        else if(Input.GetButtonUp("Sprint"))
-            runSpeed /= 1.5f;
+
+        if (isSwinging)
+        {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+            }
+            else if (Input.GetButtonDown("Sprint"))
+                runSpeed *= 1.5f;
+            else if (Input.GetButtonUp("Sprint"))
+                runSpeed /= 1.5f;
+        }
     }
 
     void FixedUpdate()
@@ -32,5 +51,11 @@ public class PlayerMovement : MonoBehaviour
         // Move our character
         controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
         jump = false;
+
+            //Move our character when climbing
+            if (isOnLadder) {
+                controller.Move(verticalMove * Time.fixedDeltaTime,jump);
+                jump = false;
+            }
     }
 }
