5bcce277a8af815455821a47a99cc0126b825113
fatherboard <hmartins@ucm.es>
2018-11-20 15:36:07 +0100

    Added falling spikes

---
+++ BobTheChameleon/Assets/Scripts/FallingSpikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikes : MonoBehaviour {
+
+    Rigidbody2D spikes;
+
+	// Use this for initialization
+	void Start () {
+        spikes = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name.Equals("Player"))
+            Invoke("MakeDynamic", 0f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("u ded");
+    }
+
+    void MakeDynamic () {
+        spikes.isKinematic = false;
+    }
+}
