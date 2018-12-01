48c3eaea15ea1b35796cf1a58491a234cf2d3c78
Riccardo Facchini <riccardo95facchini@gmail.com>
2018-12-01 20:26:28 +0100

    Better patrol script

--- BobTheChameleon/Assets/Scripts/Patrol.cs
+++ BobTheChameleon/Assets/Scripts/Patrol.cs
@@ -12,24 +12,81 @@ public class Patrol : MonoBehaviour
     [SerializeField]
     private Transform groundDetection;
 
+    public LayerMask whatIsPlayer;
+
     private bool movingLeft = true;
     private bool charging;
+
+    private Vector2 rayDirection;
+    private Transform player = null;
 
     void Update()
     {
         float speed;
 
+        HorizontalCheck();
+        VerticalCheck();
+
         if(!charging)
+        {
             speed = walkSpeed;
+            charging = PlayerInSight();
+        }
         else
+        {
             speed = chargeSpeed;
+            FollowPlayer();
+        }
 
         transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
 
+    /// <summary>
+    /// If the player jumps behind during chase, flip sprite
+    /// </summary>
+    private void FollowPlayer()
+    {
+        RaycastHit2D playerBehind = Physics2D.Raycast(transform.localPosition, -rayDirection, Mathf.Infinity, whatIsPlayer);
+
+        if(playerBehind.collider != false)
+            FlipSprite();
+    }
+
+    /// <summary>
+    /// Checks if the player is in line of sight, if so start charging
+    /// </summary>
+    private bool PlayerInSight()
+    {
+        RaycastHit2D playerInSight = Physics2D.Raycast(transform.localPosition, rayDirection, lineOfSight, whatIsPlayer);
+
+        if(playerInSight.collider != false)
+        {
+            if(player == null)
+                player = playerInSight.collider.transform;
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Checks if the enemy is about to fall from a platform and flips it if so
+    /// </summary>
+    private void VerticalCheck()
+    {
         RaycastHit2D verticalCheck = Physics2D.Raycast(groundDetection.position, Vector2.down, 0.5f);
 
-        Vector2 rayDirection;
+        if(verticalCheck.collider == false)
+        {
+            charging = false;
+            FlipSprite();
+        }
+    }
 
+    /// <summary>
+    /// Checks if the enemy is about to bump into an obstacle and flips it if so
+    /// </summary>
+    private void HorizontalCheck()
+    {
         if(movingLeft)
             rayDirection = Vector2.left;
         else
@@ -40,24 +97,16 @@ public class Patrol : MonoBehaviour
         if(horizontalCheck.collider != false)
         {
             if(horizontalCheck.collider.tag != Names.Tags.Player.ToString())
+            {
+                charging = false;
                 FlipSprite();
+            }
         }
-
-        horizontalCheck = Physics2D.Raycast(groundDetection.position, rayDirection, lineOfSight);
-
-        if(horizontalCheck.collider != false)
-        {
-            if(horizontalCheck.collider.tag == Names.Tags.Player.ToString())
-                charging = true;
-        }
-
-        if(verticalCheck.collider == false)
-            FlipSprite();
     }
 
     private void FlipSprite()
     {
-        charging = false;
+        //charging = PlayerInSight();
 
         if(movingLeft)
         {
