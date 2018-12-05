e5fef175a29d26301435d8a723aff0e01356a418
Riccardo Facchini <riccardo95facchini@gmail.com>
2018-12-05 19:22:18 +0100

    Added somewhat random movement for the idle prey and similar sight script from the eagle, unique script will be made after merge

---
+++ BobTheChameleon/Assets/Scripts/PreyPatrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PreyPatrol : MonoBehaviour
+{
+
+    private float movementRange = 2f; // TODO: take it from scriptable object
+    private float preySpeed = 5; // TODO: take it from scriptable object
+    private float newPositionInterval = 1f; // TODO: take it from scriptable object
+
+    private Vector2 startingPosition;
+    private Vector2 nextPosition;
+
+    /// <summary>
+    /// Chaching and coroutine starting
+    /// </summary>
+    private void OnEnable()
+    {
+        startingPosition = transform.position;
+        StartCoroutine(RandomMovement());
+        StartCoroutine(NewPosition());
+    }
+
+    /// <summary>
+    /// Stops random movement and new position generation, starts Flee coroutine
+    /// </summary>
+    public void StartFlee()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Flee());
+    }
+
+    /// <summary>
+    /// Moves the prey towards a random position
+    /// </summary>
+    /// <returns>null</returns>
+    private IEnumerator RandomMovement()
+    {
+        while(true)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, nextPosition, preySpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Generates a new random position in the range, each interval.
+    /// </summary>
+    /// <returns>After time newPositionInterval starts again</returns>
+    private IEnumerator NewPosition()
+    {
+        while(true)
+        {
+            nextPosition = new Vector2(
+                startingPosition.x + Random.Range(-movementRange, movementRange),
+                startingPosition.y + Random.Range(-movementRange, movementRange));
+            yield return new WaitForSeconds(newPositionInterval);
+        }
+    }
+
+    private IEnumerator Flee()
+    {
+        yield return null;
+    }
+}
---
+++ BobTheChameleon/Assets/Scripts/PreySight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PreySight : MonoBehaviour
+{
+    [SerializeField]
+    private PreyPatrol preyPatrol;
+    [SerializeField]
+    private LayerMask WhatIsObstacleAndPlayer;
+
+    private Transform playerPosition = null;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == Names.Tags.Player.ToString())
+        {
+            if(playerPosition == null)
+                playerPosition = collision.transform;
+
+            if(IsPlayerInSight())
+            {
+                preyPatrol.StartFlee();     //Calls method to start fleeing
+                gameObject.SetActive(false);//Disables trigger that serves no purpose now
+            }
+        }
+    }
+
+    /// <summary>
+    /// If player in the collision, checks if it's in sight
+    /// </summary>
+    /// <returns>ture if player is in sight, false otherwise</returns>
+    private bool IsPlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,
+                    (playerPosition.position - transform.position).normalized,
+                    Vector2.Distance(transform.position, playerPosition.position), WhatIsObstacleAndPlayer);
+
+        if(hit.collider.tag == Names.Tags.Player.ToString())
+            return true;
+        else
+            return false;
+    }
+}
