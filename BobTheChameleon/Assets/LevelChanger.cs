094f35600819df3479f5a7c72490c48322093c86
Unknown <michele.mevi@mail.polimi.it>
2019-02-14 14:25:11 +0100

    animation of the transition between different levels
    (wip on the menu between levels)

---
+++ BobTheChameleon/Assets/LevelChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelChanger : MonoBehaviour
+{
+    public Animator animator;
+    private int levelToLoad;
+    public GameObject finishingFlag;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FadeToNextLevel();
+
+        if (finishingFlag.GetComponent<NextLevel>().getCollided()) {
+        //}
+
+
+        // (Input.GetMouseButton(0)) {          //useful to quickly test the change of the level
+            FadeToNextLevel();
+        }
+    }
+
+
+    public void FadeToNextLevel() {
+        FadeToLevel(SceneManager.GetActiveScene().buildIndex+1);
+
+    }
+
+
+    public void FadeToLevel(int levelIndex) {
+
+        levelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+
+
+    public void OnFadeComplete() {
+
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
--- BobTheChameleon/Assets/Scripts/Environment/NextLevel.cs
+++ BobTheChameleon/Assets/Scripts/Environment/NextLevel.cs
@@ -4,12 +4,17 @@ public class NextLevel : MonoBehaviour
 {
     private bool collided = false;
 
+    public bool getCollided() {
+        return collided;
+    }
+    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == Names.Tags.Player.ToString())
         {
             if(!collided)
-                GameManager.Instance.LoadNext();
+               // GameManager.Instance.LoadNext();
             collided = true;
         }
     }
