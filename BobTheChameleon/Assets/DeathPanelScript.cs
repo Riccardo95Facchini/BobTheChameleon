45e57dcf890db8c31dad7520e48893480717dc07
Unknown <michele.mevi@mail.polimi.it>
2019-02-16 21:14:49 +0100

    Fixed Game Over Menu 's Menu Button

---
+++ BobTheChameleon/Assets/DeathPanelScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathPanelScript : MonoBehaviour
+{
+    public HealthManager healthManager;
+
+    public void MenuAfterDeath()
+    {
+        
+        EventManager.TriggerEvent(Names.Events.Respawn);
+        SceneManager.LoadScene("Menu");
+    }
+
+    
+}
